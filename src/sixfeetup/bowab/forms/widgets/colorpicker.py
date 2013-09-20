ac14f76592df82ac2ec01641dff611c34787e373
cewing <cris@crisewing.com>
2013-09-20 15:30:14 -0700

    color picker widget code, a simple extension of the basic text input
    refs #1980

---
+++ src/sixfeetup/bowab/forms/widgets/colorpicker.py
@@ -0,0 +1,31 @@
+from deform.widget import ResourceRegistry
+from deform.widget import TextInputWidget
+
+
+spectrum_resources = {
+    "colorpicker": {'spectrum-1.1.1': {'js': ('spectrum.js', ),
+                                       'css': ('spectrum.css', )}},
+}
+
+
+# set a resource registry that contains resources for the password widget
+colorpicker_registry = ResourceRegistry()
+colorpicker_registry.set_js_resources('colorpicker', 'spectrum-1.1.1',
+                                      'spectrum.js')
+colorpicker_registry.set_css_resources('colorpicker', 'spectrum-1.1.1',
+                                    'spectrum.css')
+
+class SpectrumColorPickerWidget(TextInputWidget):
+    requirements = (('colorpicker', 'spectrum-1.1.1'), )
+    template = 'sixfeetup.bowab:templates/widgets/colorpicker'
+    color = "#ff0000"
+    default = None
+
+
+def build_color_widget(color, default):
+    """build a widget by passing in specific settings
+    
+    this can be used in a deferred widget binding to allow for a variety of
+    widgets on a single page with different settings for color and default
+    """
+    return SpectrumColorPickerWidget(color=color, default=default)
