f8822249e4a94130fedec75eab764ddfdb04c48f
cewing <cris@crisewing.com>
2013-09-20 15:33:24 -0700

    pull new symbols from the colorpicker widget in the widgets namespace, allows for simpler imports
    refs #1980

--- src/sixfeetup/bowab/forms/widgets/__init__.py
+++ src/sixfeetup/bowab/forms/widgets/__init__.py
@@ -1,3 +1,5 @@
 from sixfeetup.bowab.forms.widgets.csrf import CSRFSchema
-
 from sixfeetup.bowab.forms.widgets.recaptcha import deferred_recaptcha_widget
+from sixfeetup.bowab.forms.widgets.colorpicker import SpectrumColorPickerWidget
+from sixfeetup.bowab.forms.widgets.colorpicker import build_color_widget
+from sixfeetup.bowab.forms.widgets.colorpicker import colorpicker_registry
